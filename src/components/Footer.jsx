529c8fb8f79a89efabeeb4996d14e18331626e47
bahy <azzougbahy@gmail.com>
2023-03-05 21:38:08 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../components/Footer.css'
 function Footer() {
     return (
         <div className='footer'>
-            <p>Crafted by the talented designer <a href="">bahy</a>, and flawlessly deployed by <a href="">Vercel</a>.</p>
+            <p>Crafted by the holy moly designer <a href="">bahy</a>, and flawlessly deployed by <a href="">Vercel</a>.</p>
             <div className="icons">
                 <a href=""><img src="src/assets/githubf.svg" alt="" /></a>
                 <div></div>

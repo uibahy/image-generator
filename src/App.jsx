1fc8227b38b337de875278170b049b790384f137
bahy <azzougbahy@gmail.com>
2023-03-05 20:32:58 +0100

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,13 @@ function App() {
 
   const loading = <ContentLoader
     speed={1}
-    width={512}
-    height={512}
-    viewBox="0 0 512 512"
+    width={256}
+    height={256}
+    viewBox="0 0 256 256"
     backgroundColor="#171717"
     foregroundColor="#1f1f1f"
   >
-    <rect x="0" y="0" rx="16" ry="16" width="512" height="512" />
+    <rect x="0" y="0" rx="16" ry="16" width="256" height="256" />
   </ContentLoader>
 
   async function fetchData() {
@@ -33,7 +33,7 @@ function App() {
       const res = await openai.createImage({
         prompt: prompt,
         n: 1,
-        size: "512x512",
+        size: "256x256",
       })
       setImage(res.data.data[0].url)
       setIsloading(false)
@@ -49,12 +49,19 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <h1>Image Generator</h1>
-        <input placeholder='Hmm?' type="text" onChange={(e) => setPrompt(e.target.value)} />
-        <button onClick={fetchData}>Generate</button>
+      <div className='container'>
+        <div className="header">
+          <h1>Image Generator</h1>
+          <p>Transform words into visuals with AI image generator. Effortlessly create custom images based on your descriptions.</p>
+        </div>
+        <div className="inputs">
+          <input placeholder='Hmm?' type="text" onChange={(e) => setPrompt(e.target.value)} />
+          <button onClick={fetchData}>Generate</button>
+        </div>
+        <div className="image">
+          {isLoading ? <>{loading}</> : <img src={image} alt="" />}
+        </div>
       </div>
-      {isLoading ? <>{loading}</> : <img src={image} alt="" />}
     </div>
   )
 }
